4ae4a9694d057c587209ae6b4dafe4ca269204b3
jaleesbajwa55 <jaleesbajwa55@gmail.com>
2025-02-17 02:14:34 -0500

    Updated port to host backend on server

--- backend/backend.py
+++ backend/backend.py
@@ -47,4 +47,4 @@ def generate_tasks():
     return jsonify({"subtasks": subtasks})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)), debug=True)
