be74c1a28a94a9dad341b3a6a05f71687db86a1a
jaleesbajwa55 <jaleesbajwa55@gmail.com>
2025-02-17 03:46:13 -0500

    Updates

---
+++ precache-manifest.c8951df0034e33603fd9efb47544f262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a9ed0a23c9d1a6ed9facdae3a5a42c",
+    "url": "/StepWise/index.html"
+  },
+  {
+    "revision": "09f4079c023f56c3b43f",
+    "url": "/StepWise/static/css/2.a3b508c3.chunk.css"
+  },
+  {
+    "revision": "fd6435e39ad689410cd1",
+    "url": "/StepWise/static/css/main.96b7cbf5.chunk.css"
+  },
+  {
+    "revision": "09f4079c023f56c3b43f",
+    "url": "/StepWise/static/js/2.8494dd82.chunk.js"
+  },
+  {
+    "revision": "fd6435e39ad689410cd1",
+    "url": "/StepWise/static/js/main.45389bb7.chunk.js"
+  },
+  {
+    "revision": "0133f2302d75f602029f",
+    "url": "/StepWise/static/js/runtime~main.39820641.js"
+  },
+  {
+    "revision": "bfb9d52c03a3906a23943aee7cbba19c",
+    "url": "/StepWise/static/media/logo.bfb9d52c.png"
+  }
+]);
